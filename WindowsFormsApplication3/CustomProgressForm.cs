5947cdc190ed2dde65668fc68b5a92f8f7cb8a47
mar Jinn <marjinn@live.com>
2013-02-21 07:49:56 +0530

    DEmo C # app

---
+++ WindowsFormsApplication3/CustomProgressForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class CustomProgressForm : Form
+    {
+        public CustomProgressForm()
+        {
+            InitializeComponent();
+
+            //get working area to launch the form at the right most corner 
+            Rectangle r = Screen.PrimaryScreen.WorkingArea;
+            //this.StartPosition = FormStartPosition.Manual;
+            this.Location = new Point((Screen.PrimaryScreen.WorkingArea.Width - this.Width) - 10 , Screen.PrimaryScreen.WorkingArea.Height - this.Height - 100 );
+
+           // MessageBox.Show((Screen.PrimaryScreen.WorkingArea.Width - this.Width - 10).ToString());
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("buhahauuahauahaauahau");
+            this.Close();
+            this.Dispose();
+        }
+    }
+}
---
+++ WindowsFormsApplication3/SchedulerForm.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class SchedulerForm : Form
+    {
+        public SchedulerForm()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            this.Dispose();
+        }
+        CustomProgressForm frm = new CustomProgressForm();
+        NotificationSplash notSplashForm = new NotificationSplash();
+
+
+
+        private void Save_Schedule_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                frm.Show();
+                notSplashForm.ShowDialog();
+
+            }
+            catch (System.ObjectDisposedException)
+            {
+                CustomProgressForm frm = new CustomProgressForm();
+                frm.ShowDialog();
+            }
+
+            finally
+            {
+
+            }
+            string dateTimePickerText = dateTimePicker1.Text.ToString();
+            string dateTimePickerTimeText = dateTimePicker2.Text.ToString();
+            string checkedRadioButton = "";
+            if (radioButtonMonthly.Checked == true)
+            {
+                checkedRadioButton = radioButtonMonthly.Text;
+            }
+
+
+
+            if (radioButtonWeekly.Checked == true)
+            {
+                checkedRadioButton = radioButtonWeekly.Text;
+            }
+
+
+
+            if (radioButtonDaily.Checked == true)
+            {
+                checkedRadioButton = radioButtonDaily.Text;
+            }
+
+
+
+            if (radioButtonDateOfChoice.Checked == true)
+            {
+                checkedRadioButton = radioButtonDateOfChoice.Text;
+            }
+
+            MessageBox.Show("Date : " + dateTimePickerText + " " + "\nTime : " + dateTimePickerTimeText + "\nSchedule Chosen : " + checkedRadioButton.ToUpper());
+            Schedule.Text = "Date : " + dateTimePickerText + " " + "\nTime : " + dateTimePickerTimeText + "\nSchedule Chosen :" + checkedRadioButton.ToUpper();
+
+        }
+    }
+}
